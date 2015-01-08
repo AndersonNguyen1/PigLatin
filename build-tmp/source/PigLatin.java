876453ca0a5768c6d85bd6083b6bad53b994b1db
unknown <annguyen1@697-334-Erebor.sf.k12.internal>
2015-01-08 11:35:41 -0800

    Finished PigLatin

--- build-tmp/source/PigLatin.java
+++ build-tmp/source/PigLatin.java
@@ -1,4 +1,22 @@
-import java.util.*;
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class PigLatin extends PApplet {
+
+
 
 public void setup() {
 	String lines[] = loadStrings("words.txt");
@@ -35,4 +53,13 @@ public String pigLatin(String sWord)
 	else if (findFirstVowel(sWord) == -2) {return sWord.substring(2, sWord.length()) + "quay";}
 	else if (findFirstVowel(sWord) == -1) {return sWord + "ay";}
 	else {return "ERROR!";}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "PigLatin" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
